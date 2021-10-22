4dce67c737dc3dfb181927492441d62a5f34f900
Александр Кононов <mister.San4es@list.ru>
2021-10-22 07:24:36 +0400

    21.10.21

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+const accordeon = () => {
+  const elements = document.querySelectorAll('.accordeon .element');
+  const element = document.querySelector('.accordeon');
+
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      let target = event.target.closest('.element');
+      if(target.classList.contains('active')){
+        target.classList.remove('active');
+      } else {
+        elements.forEach(elem => {
+          elem.classList.remove('active');
+        });
+        target.classList.add('active');
+      }
+    });
+};
+accordeon();
